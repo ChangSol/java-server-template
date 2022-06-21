891b8a9a445f8e703eb8215b69126276348e7168
ChangSol <ckdwnsla12@naver.com>
2022-06-21 10:30:14 +0900

    Bean 정의 및 시큐리티 변경

--- src/main/java/org/changsol/api/security/support/RestAuthenticationEntryPoint.java
+++ src/main/java/org/changsol/api/security/support/RestAuthenticationEntryPoint.java
@@ -2,16 +2,20 @@ package org.changsol.api.security.support;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+/**
+ * Spring Security에서 Exception이 발생했을 때 클래스
+ */
+@Component
 public class RestAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
-  @Override
-  public void commence(HttpServletRequest request, HttpServletResponse response,
-      AuthenticationException authException) throws IOException {
-    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-  }
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 }
--- src/main/java/org/changsol/api/security/support/RestAuthenticationFailureHandler.java
+++ src/main/java/org/changsol/api/security/support/RestAuthenticationFailureHandler.java
@@ -2,19 +2,17 @@ package org.changsol.api.security.support;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 public class RestAuthenticationFailureHandler implements AuthenticationFailureHandler {
 
-  @Override
-  public void onAuthenticationFailure(
-      HttpServletRequest request,
-      HttpServletResponse response,
-      AuthenticationException exception
-  ) throws IOException {
-    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
-  }
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
 }
