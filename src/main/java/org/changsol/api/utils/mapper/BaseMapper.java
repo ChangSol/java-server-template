fb74bbc9bc73496ac949eeebef87bb8a39a1c3a2
ChangSol <ckdwnsla12@naver.com>
2023-01-31 23:04:00 +0900

    Base Mapper 제거

--- src/main/java/org/changsol/api/utils/mapper/BaseMapper.java
+++
@@ -1,9 +0,0 @@
-package org.changsol.api.utils.mapper;
-
-import org.mapstruct.MappingTarget;
-
-public interface BaseMapper<E, R, C, U> {
-    R toResponse(E entity);
-    E entityCreate(C createDto);
-    void entityUpdate(E sampleMaster, @MappingTarget U updateDto);
-}
