ff2e6fb43e20291087293063bde94e3fe1443417
ChangSol <ckdwnsla12@naver.com>
2023-02-02 14:01:03 +0900

    featrue SampleMaster edit

--- src/main/java/org/changsol/api/apps/samples/repository/SampleMasterRepository.java
+++ src/main/java/org/changsol/api/apps/samples/repository/SampleMasterRepository.java
@@ -1,5 +1,6 @@
 package org.changsol.api.apps.samples.repository;
 
+import org.changsol.api.apps.bases.repository.BaseRepository;
 import org.changsol.api.apps.samples.entity.SampleMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -9,6 +10,6 @@ import org.springframework.stereotype.Repository;
  * Sample Master Repo Class
  */
 @Repository
-public interface SampleMasterRepository extends JpaRepository<SampleMaster, Long>, JpaSpecificationExecutor<SampleMaster> {
+public interface SampleMasterRepository extends BaseRepository<SampleMaster, Long> {
 
 }
